c4fd944333814f0d233504a21482b1ca9a94ffe6
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-06-24 16:16:39 +0200

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PhotosComponent } from './components/photos/photos.component';
+import { UploadComponent } from './components/upload/upload.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhotosComponent,
+    UploadComponent
   ],
   imports: [
     BrowserModule,

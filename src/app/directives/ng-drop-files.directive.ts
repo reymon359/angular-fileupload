0fc554ca432e6cc7911ed1fa0d3024407145b828
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-06-28 11:10:54 +0200

    starting validations

--- src/app/directives/ng-drop-files.directive.ts
+++ src/app/directives/ng-drop-files.directive.ts
@@ -1,4 +1,5 @@
 import { Directive, EventEmitter, ElementRef, HostListener, Input, Output } from '@angular/core';
+import { FileItem } from '../models/file-item';
 // EventEmitter to comunicate with father
 // ElementRef to obtain info from the html element where it is
 // Hostlistener to listen to events like mouseover
@@ -6,6 +7,9 @@ import { Directive, EventEmitter, ElementRef, HostListener, Input, Output } from
   selector: '[appNgDropFiles]'
 })
 export class NgDropFilesDirective {
+
+  // The files I will receive
+  @Input() files: FileItem[] = [];
 
   // To comunicate the event to father
   @Output() mouseOver: EventEmitter<boolean> = new EventEmitter();
@@ -17,6 +21,8 @@ export class NgDropFilesDirective {
   @HostListener('dragover', ['$event'])
   public onDragEnter(event: any) {
     this.mouseOver.emit(true);
+    this._preventStop(event);
+
   }
 
   // to emit an event 
@@ -25,5 +31,42 @@ export class NgDropFilesDirective {
     this.mouseOver.emit(false);
   }
 
+  // to emit an event on drop 
+  @HostListener('drop', ['$event'])
+  public onDrop(event: any) {
 
+    const transference = this._getTransference(event);
+
+    // If there is nothing to transfer I exit
+    if (!transference) { return; }
+
+    this._extractFiles(transference.files);
+
+    this._preventStop(event);
+
+    this.mouseOver.emit(false);
+
+  }
+
+  // For browser compatibility
+  private _getTransference(event: any) {
+    return event.dataTransfer ? event.dataTransfer : event.originalEvent.dataTransfer;
+  }
+
+  // We will create and array with the files dropped
+  private _extractFiles(listFiles: FileList) {
+    // tslint:disable-next-line:forin
+    for (const property in Object.getOwnPropertyNames(listFiles)) {
+      const tempFile = listFiles[property];
+      if (this._fileCanBeUploaded(tempFile)) {
+        const newFile = new FileItem(tempFile);
+        this.files.push(newFile);
+      }
+    }
+  }
+
+
+
+ 
 }
+

497899413b9bcb64f49cd0d69f6fc939fdfe7b94
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-06-28 12:04:40 +0200

    upload works

--- src/app/services/image-upload.service.ts
+++ src/app/services/image-upload.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-
 // Firebase stuff
+
 import { AngularFirestore } from '@angular/fire/firestore';
 import * as firebase from 'firebase';
 import { FileItem } from '../models/file-item';
@@ -16,7 +16,43 @@ export class ImageUploadService {
   constructor(private db: AngularFirestore) { }
 
   uploadImagesFirebase(images: FileItem[]) {
-    console.log(images);
+
+    // Reference with storage collection 
+    const storageRef = firebase.storage().ref();
+
+    for (const item of images) {
+      item.uploading = true;
+      if (item.progress >= 100) {
+        continue;
+      }
+
+      const uploadTask: firebase.storage.UploadTask =
+        storageRef.child(`${this.IMAGES_FOLDER}/${item.fileName}`)
+          .put(item.file);
+
+      uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED,
+        (snapshot: firebase.storage.UploadTaskSnapshot) => item.progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100,
+        (error) => console.error('Error uploading', error),
+        // If everything goes ok
+        () => {
+          console.log('Image uploaded succesfully');
+          uploadTask.snapshot.ref.getDownloadURL().then(
+            (onfullfilled: any) => {
+              console.log('(promise) the download url is:  ' + onfullfilled);
+              item.url = onfullfilled;
+              console.log(item.url);
+              item.uploading = false;
+              this.saveImage({
+                name: item.fileName,
+                url: item.url
+              });
+            },
+            (onrejected: any) => {
+              console.log('(promise) the download url has been rejected');
+            });
+        }
+      );
+    }
   }
 
 

0c317b02b97ab4680db4c8a5266f603c787c6106
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-06-28 11:17:29 +0200

    displaying files on table

--- src/app/components/upload/upload.component.ts
+++ src/app/components/upload/upload.component.ts
@@ -21,4 +21,8 @@ export class UploadComponent implements OnInit {
   uploadImages() {
   this.imageUploadService.uploadImagesFirebase(this.files); 
   }
+
+  cleanFiles(){
+    this.files = [];
+  }
 }
